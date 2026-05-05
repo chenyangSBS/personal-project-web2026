39bc219610003134bd5edf17620e51e9ec46de79
chenyangSBS <chenyang@sbs.edu.cn>
2026-05-06 02:24:35 +0800

    update assignment

---
+++ src/main/java/cs/sbs/web/personalprojectweb2026/PersonalProjectWeb2026Application.java
@@ -0,0 +1,13 @@
+package cs.sbs.web.personalprojectweb2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectWeb2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalProjectWeb2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/cs/sbs/web/personalprojectweb2026/PersonalProjectWeb2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.sbs.web.personalprojectweb2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectWeb2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
